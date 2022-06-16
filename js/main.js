9f93d1069056cc7f7236110ca6213939ffa80d70
abdumannab-usmanov <usmanovabdumannab2307@gmail.com>
2022-06-16 23:28:20 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,79 @@
+// ========1-masala===========
+function addition(a, b) {
+	return a + b
+	
+}
+
+// ==============2-masala============
+function convert(minutes) {
+	return (minutes * 60)
+}
+
+// ===============3-masala===============
+function addition(num) {
+	return ++num
+}
+// addition(0) ➞ 1
+// addition(9) ➞ 10
+// addition(-3) ➞ -2
+
+
+// ==========4-masala===========
+function triArea(base, height) {
+	return (base * height) / 2
+}
+
+// triArea(3, 2) ➞ 3
+// triArea(7, 4) ➞ 14
+// triArea(10, 10) ➞ 50
+
+
+// ==================5-masala===============
+function calcAge(age) {
+	return age * 365
+}
+
+// ================6-masala=================
+
+function remainder(x, y) {
+	return x % y
+}
+
+// remainder(1, 3) ➞ 1
+// remainder(3, 4) ➞ 3 
+// remainder(-9, 45) ➞ -9
+// remainder(5, 5) ➞ 0
+
+
+// ==============7-masala==================
+
+function footballPoints(wins, draws, losses) {
+	return wins * 3 + draws
+}
+
+// footballPoints(3, 4, 2) ➞ 13
+// footballPoints(5, 0, 2) ➞ 15
+// footballPoints(0, 0, 1) ➞ 0
+
+// ===========8-masala===============
+
+function points(twoPointers, threePointers) {
+	return twoPointers * 2 + threePointers * 3
+}
+
+// ===============9-masala===============
+
+function lessThan100(a, b) {
+	if (a + b < 100){
+		   return true;
+		  }else{
+			  return false
+		  }
+}
+
+// ====================10-masala================
+
+function animals(chickens, cows, pigs) {
+	return chickens * 2 + cows * 4 + pigs * 4 
+}
+
